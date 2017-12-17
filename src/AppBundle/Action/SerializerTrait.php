3c44cabe7df177de3328b93253323aa7ada7ed3b
Michal H <harezlak.dev@gmail.com>
2017-12-17 17:14:27 +0100

    Simple serializer and validator trait

---
+++ src/AppBundle/Action/SerializerTrait.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Action;
+
+use JMS\Serializer\Context;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\Exception\ObjectConstructionException;
+use JMS\Serializer\Exception\ValidationFailedException;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Constraints\Required;
+
+/**
+ * Serializer Trait
+ */
+trait SerializerTrait
+{
+    /**
+     * Serializer
+     *
+     * @var SerializerInterface
+     */
+    protected $serializer;
+
+    /**
+     * Set dependency
+     *
+     * @Required
+     *
+     * @ignore
+     * @param SerializerInterface $serializer
+     */
+    public final function setSerializer(SerializerInterface $serializer): void
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Serialize to JSON
+     *
+     * @param object $data
+     * @param string[] $groups
+     *
+     * @return string
+     */
+    protected function serialize($data, array $groups = ["full"]): string
+    {
+        $context = $this->createSerializationContext($groups);
+
+        $json = $this->serializer->serialize($data, 'json', $context);
+
+        return $json;
+    }
+
+    /**
+     * Deserialize JSON data
+     *
+     * @param string $json
+     * @param string[] $groups
+     * 
+     * @return object
+     *
+     * @throws BadRequesApiHttpException
+     */
+    protected function deserialize(string $json, array $groups = ["full"])
+    {
+        $context = $this->createDeserializationContext($groups);
+
+        $data = null;
+        try {
+            $data = $this->serializer->deserialize($json, $this->getDataClass(), 'json',
+                $context);
+        } catch (JmsRuntimeException $e) {
+            throw new BadRequesApiHttpException($e->getMessage());
+        } catch (ObjectConstructionException $e) {
+            throw new BadRequesApiHttpException($e->getMessage());
+        } catch (ValidationFailedException $e) {
+            throw new BadRequesApiHttpException($e->getMessage());
+        } catch (\Exception $e) {
+            throw new BadRequesApiHttpException('Could not deserialize data');
+        }
+
+        return $data;
+    }
+
+    /**
+     * Create deserialization context
+     *
+     * @param string[] $groups
+     * 
+     * @return Context
+     */
+    protected function createDeserializationContext(array $groups = []): Context
+    {
+        $context = DeserializationContext::create();
+        $context->setGroups($groups);
+        return $context;
+    }
+
+    /**
+     * Create serialization context
+     *
+     * @param string[] $groups
+     *
+     * @return Context
+     */
+    protected function createSerializationContext(array $groups = []): Context
+    {
+        $context = SerializationContext::create();
+        $context->setGroups($groups);
+        return $context;
+    }
+}
---
+++ src/AppBundle/Action/ValidatorTrait.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Action;
+
+use AppBundle\Exception\BadRequestApiException;
+use Symfony\Component\Validator\Constraints\Required;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Serializer Trait
+ */
+trait ValidatorTrait
+{
+    /**
+     * Validator
+     *
+     * @var ValidatorInterface
+     */
+    protected $validator;
+
+    /**
+     * Set dependency
+     *
+     * @Required
+     *
+     * @ignore
+     * @param ValidatorInterface $validator
+     */
+    public final function setValidator(ValidatorInterface $validator): void
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * Validate object
+     * Throws exception if constraint violations exist
+     *
+     * @param object $data
+     * @param string[] $groups
+     *
+     * @throws BadRequesApiHttpException
+     */
+    protected function validate($data): void
+    {
+        /* @var $violations ConstraintViolationListInterface */
+        /* @var $violation ConstraintViolationInterface */
+        $violations = $this->validator->validate($data);
+        if ($violations->count() > 0) {
+            $messages = '';
+            foreach ($violations as $violation) {
+                $messages .= sprintf('Invalid property "%s". %s ', $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new BadRequestApiException(trim($messages));
+        }
+    }
+}
